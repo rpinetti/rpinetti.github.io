030625d631171569c69c68b7a4e54afcbddd9217
Roberto Martinelli Pinetti <robertopinetti@gmail.com>
2020-05-25 12:44:20 -0300

    Tradução para o pt-BR

--- Assets/js/App.js
+++ Assets/js/App.js
@@ -1,22 +1,24 @@
-$( document ).ready( function() {
-	var i18n = $.i18n( );
-	// Use default locale 'pt-br'
-//	i18n.locale = 'pt-BR';
-	i18n.load('./Assets/i18n/lang.json', i18n.locale);
-	$( '.uls-trigger' ).uls( {
-		onReady: function ( ) {
-			this.i18n();
-		},
-		onSelect: function() {
-			var languageName = $.uls.data.getAutonym( language );
-			$( '.uls-trigger' ).text( languageName );
-			i18n.locale = language;
-			i18n.load('./Assets/i18n/lang.json', language);
-			// re-i18n the elements
-			this.i18n();
-			$( '.container' ).i18n();
-		},
-		quickList: ['pt-BR', 'en', 'es', 'it']
-	});	// $( '.uls-trigger' ).uls
+/**
+ * Busca a tradução do texto no diretório i18n
+ * API: https://github.com/wikimedia/jquery.i18n
+ * DOC: https://lokalise.com/blog/localizing-apps-jquery/
+ */
+function updateText() {
+	'use strict';
+	var i18n = $.i18n();
+	let i18nFile = `./Assets/i18n/${$.i18n().locale}.json`;
 
-});	// $( document ).ready
+	fetch(i18nFile).then((response) => response.json()).then((messages) => {
+		$.i18n().load(messages, $.i18n().locale);
+		$('html').i18n();	// to translate the element matching jquery selector based on data-i18n key
+	});
+
+}	// function updateText() 
+
+// Enable debug
+$.i18n.debug = true;
+
+$( document ).ready( function ( $ ) {
+	'use strict';
+	updateText();
+});
