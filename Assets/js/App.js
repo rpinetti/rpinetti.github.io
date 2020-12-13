91d89b3e36cab15d8636016b6b26d6e9fd67231f
Roberto Martinelli Pinetti <robertopinetti@gmail.com>
2020-12-13 02:01:56 -0300

    Ajuste no layout para utilização do flexbox e definição das cores

--- Assets/js/App.js
+++ Assets/js/App.js
@@ -1,14 +1,29 @@
-let auth_github = 'a05de5bdb6aa3f142afbd1f910726f343e15da1c';
+let auth_github = 'd4a2dc2e2d1231544d3033478830be68e452441a';
 
 // Enable debug
 $.i18n.debug = true;
 
 /**
+ * AJAX activity indicator bound to ajax start/stop document events
  * 
+ * @event ajaxStart
  */
-$( document ).ready( function ( $ ) {
+$(document).ajaxStart(function () {
+	$('#ajaxBusy').show();
+}).ajaxStop(function () {
+	$('#ajaxBusy').hide();
+});
+
+// Set up the AJAX indicator
+$('body').append('<div id="ajaxBusy" class="ajaxBusy"><p id="ajaxBusyMsg">Please wait...</p></div>');
+
+/**
+ * 
+ * @event ready
+ */
+$(document).ready(function ($) {
 	listExperience();
-	// 
+	// Translate Texts
 	updateText();
 	// Request Github API for get Perfil
 	readPerfil();
@@ -20,34 +35,42 @@ $( document ).ready( function ( $ ) {
  * Read the i18n files for website translation
  * API: https://github.com/wikimedia/jquery.i18n
  * DOC: https://lokalise.com/blog/localizing-apps-jquery/
+ * 
+ * @function updateText
  */
 function updateText() {
 	'use strict';
 	var i18n = $.i18n();
-	let languages = ['pt-BR', 'en', 'it', 'es', 'no'];
+	let languages = ['pt-BR', 'pt', 'en'];
 
 	// set default language
-	if (!languages.find( element => element === $.i18n().locale )) {
-		$.i18n().locale = 'en';
-	}
+	if ( $.i18n().locale === 'pt' || $.i18n().locale === 'pt-BR' ) {
+		$.i18n().locale = 'pt-BR';
+	} else {
+		if (!languages.find(element => element === $.i18n().locale)) {
+			$.i18n().locale = 'en';
+		}
+	}	// if ( $.i18n().locale === 'pt' ||
 
 	let i18nFile = `./Assets/i18n/${$.i18n().locale}.json`;
 
 	fetch(i18nFile).then((response) => response.json()).then((messages) => {
 		$.i18n().load(messages, $.i18n().locale);
-		$('html').i18n();	// to translate the element matching jquery selector based on data-i18n key
+		$('html').i18n(); // to translate the element matching jquery selector based on data-i18n key
 	});
 
-}	// function updateText() 
+} // function updateText() 
 
 /**
  * Create Expiriences
+ * 
+ * @function addExperience
  * @param String _year 
  * @param String _title 
  * @param String _text 
  * @returns String divLine
  */
-function addExperience(_year, _title, _text){
+function addExperience(_year, _title, _text) {
 	var divLine = "<div class='line'>";
 	divLine += "<div class='left'>";
 	divLine += "<div class='year'><p class='title'>" + _year + "</p></div>";
@@ -56,30 +79,34 @@ function addExperience(_year, _title, _text){
 	divLine += "</div></div>";
 
 	return divLine;
-}	// function addExperience
+} // function addExperience
 
 /**
  *
+ * @function listExperience
  */
 function listExperience() {
 	// Col1
 	$('#xpCol1').append(addExperience('2020', 'Engineering Brazil', 'xp1-2020-1'));
-	$('#xpCol1').append(addExperience('2020', 'Engineering Brazil', 'xp1-2020-1'));
-	$('#xpCol1').append(addExperience('2020', 'Engineering Brazil', 'xp1-2020-1'));
-	$('#xpCol1').append(addExperience('2020', 'Engineering Brazil', 'xp1-2020-1'));
-	$('#xpCol1').append(addExperience('2020', 'Engineering Brazil', 'xp1-2020-1'));
-	$('#xpCol1').append(addExperience('2020', 'Engineering Brazil', 'xp1-2020-1'));
-	$('#xpCol1').append(addExperience('2020', 'Engineering Brazil', 'xp1-2020-1'));
+	$('#xpCol1').append(addExperience('2019', 'Evox Corp', 'xp1-2019-2'));
+	$('#xpCol1').append(addExperience('2019', 'Evt IT', 'xp1-2019-1'));
+	$('#xpCol1').append(addExperience('2018', 'HCMX', 'xp1-2018-1'));
+	$('#xpCol1').append(addExperience('2017', 'Lafarge Holcim', 'xp1-2017-2'));
+	$('#xpCol1').append(addExperience('2017', 'Walmart', 'xp1-2017-1'));
+	$('#xpCol1').append(addExperience('2016', 'Neoris', 'xp1-2016-1'));
 	// Col2
-	$('#xpCol2').append(addExperience('2020', 'Engineering Brazil', 'xp1-2020-1'));
-	$('#xpCol2').append(addExperience('2020', 'Engineering Brazil', 'xp1-2020-1'));
-	$('#xpCol2').append(addExperience('2020', 'Engineering Brazil', 'xp1-2020-1'));
-	$('#xpCol2').append(addExperience('2020', 'Engineering Brazil', 'xp1-2020-1'));
-	$('#xpCol2').append(addExperience('2020', 'Engineering Brazil', 'xp1-2020-1'));
-	$('#xpCol2').append(addExperience('2020', 'Engineering Brazil', 'xp1-2020-1'));
-	$('#xpCol2').append(addExperience('2020', 'Engineering Brazil', 'xp1-2020-1'));
-}	// function listExperience
+	$('#xpCol2').append(addExperience('2015', 'Alcis', 'xp1-2015-1'));
+	$('#xpCol2').append(addExperience('2014', 'Agile', 'xp1-2014-1'));
+	$('#xpCol2').append(addExperience('2011', 'Decisiva Consultoria', 'xp1-2011-1'));
+	$('#xpCol2').append(addExperience('2010', 'Assurance IT', 'xp1-2010-2'));
+	$('#xpCol2').append(addExperience('2010', 'Plamarc', 'xp1-2010-1'));
+	$('#xpCol2').append(addExperience('2008', 'Accenture Brazil', 'xp1-2008-1'));
+} // function listExperience
 
+/**
+ * 
+ * @ onBurgerMenu
+ */
 function onBurgerMenu() {
 	var menu = document.getElementById("navBar");
 	menu.style.display = (menu.style.display === "block") ? "none" : "block";
@@ -87,6 +114,8 @@ function onBurgerMenu() {
 
 /**
  * Request Github API for get perfil
+ * 
+ * @function readPerfil
  */
 function readPerfil() {
 	$.ajax({
@@ -95,20 +124,22 @@ function readPerfil() {
 		dataType: 'json',
 		crossDomain: true,
 		authorization: this.auth_github
-	}).done(function(response){
+	}).done(function (response) {
 		console.log("Github Perfil - success");
 		console.log(response);
 		addPhoto(response.avatar_url);
-	}).fail(function(response){
+	}).fail(function (response) {
 		console.log("Github Perfil - error");
 		console.log(response);
-	}).always(function(){
+	}).always(function () {
 		console.log("Github Perfil - complete");
 	});
-}	// readPerfil()
+} // readPerfil()
 
 /**
  * Request Github API for list repositories
+ * 
+ * @function readRepository
  */
 function readRepository() {
 	$.ajax({
@@ -117,23 +148,31 @@ function readRepository() {
 		dataType: 'json',
 		crossDomain: true,
 		authorization: this.auth_github
-	}).done(function(response){
+	}).done(function (response) {
 		console.log("Github Repos - success");
 		console.log(response);
-	}).fail(function(){
+	}).fail(function () {
 		console.log("Github Repos - error");
-	}).always(function(){
+	}).always(function () {
 		console.log("Github Repos - complete");
 	});
-}	// readRepository()
+} // readRepository()
 
 /**
  * 
+ * @function addPhoto
  * @param {*} _url 
  */
 function addPhoto(_url) {
-	var img = document.createElement("img");
-	img.src = `<img src="${_url}"></img>`;
+	var img = new Image();
+	var picture = document.getElementById('myphoto');
 
-	$( 'myphoto' ).appendChild(img);
-}	// addPhoto()
+	img.src = _url != '' ? _url : 'https://avatars3.githubusercontent.com/u/12352383?v=4';
+	img.className = 'avatar';
+	img.id = 'avatar';
+
+	img.onload = function () {
+		picture.appendChild(img);
+	}
+
+} // addPhoto()
--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 ## Third Party components
 - **JQuery i18n:** https://github.com/wikimedia/jquery.i18n
 - **Icons:** https://br.freepik.com/vetores-gratis/icones-de-midia-social_1205354.htm
+- **Bootstrap Icons:** https://github.com/twbs/icons 
 
 ## Inspirations
 - **Layout:** https://developer-cv.weblium.site/
@@ -9,3 +10,10 @@
 - **Flexbox:** https://flexbox.ninja
 - **Tableless:** https://tableless.com.br/centralizando-conteudo-na-vertical-e-horizontal-com-css-flexbox/
 - **Mozilla Doc:** https://developer.mozilla.org/
+
+## Colors
+.Paleta-inteligento-em-cinza-1-rgba { color: rgba(44, 64, 89, 1); }
+.Paleta-inteligento-em-cinza-2-rgba { color: rgba(113, 123, 140, 1); }
+.Paleta-inteligento-em-cinza-3-rgba { color: rgba(211, 215, 220, 1); }
+.Paleta-inteligento-em-cinza-4-rgba { color: rgba(47, 54, 62, 1); }
+.Paleta-inteligento-em-cinza-5-rgba { color: rgba(88, 155, 167, 1); }
